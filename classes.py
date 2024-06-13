08f73b1646918374d669b8e8e3fe19d4715a706d
Emads <emadsaab722@gmail.com>
2024-06-13 19:56:38 +0300

    complete Temperature.get() method

--- classes.py
+++ classes.py
@@ -1,3 +1,13 @@
+import requests
+import random
+from selectorlib import Extractor
+from constants import *
+
+
+class ElementException(Exception):
+    pass
+
+
 class Calorie:
 
     def __init__(self, weight, height, age, temperature) -> None:
@@ -11,10 +21,87 @@ class Calorie:
 
 
 class Temperature:
+    """
+    Represents a temperature value extracted from the `https://www.timeanddate.com/weather` webpage.
 
-    def __init__(self, country, city) -> None:
-        self.country = country
-        self.city = city
+    Attributes:
+        base_endpoint (str): The base URL for the weather information.
+        country (str): The country for which to get the temperature, in lowercase.
+        city (str): The city for which to get the temperature, in lowercase.
+        url (str): The constructed URL for the weather information for the specified country and city.
+    """
+    base_endpoint = "https://www.timeanddate.com/weather"
 
-    def get(self):
-        pass
+    def __init__(self, country: str, city: str) -> None:
+        """
+        Initializes the Temperature object with a specific country and city.
+
+        Args:
+            country (str): The country for which to get the temperature.
+            city (str): The city for which to get the temperature.
+        """
+        self.country = country.lower()
+        self.city = city.lower()
+        self.url = f"{self.base_endpoint}/{self.country}/{self.city}"
+
+    def get(self) -> (float, str):
+        """
+        Retrieves the current temperature for the specified country and city.
+
+        Returns:
+            tuple: A tuple containing the current temperature in degrees (assumed to be Celsius) and an error message.
+                   If the temperature is successfully retrieved, the error message will be None.
+                   If an error occurs, the temperature will be None and the error message will provide details.
+
+        Raises:
+            Exception: If an error occurs during the HTTP request or data extraction.
+        """
+        try:
+            # Define headers for the HTTP request to mimic a browser request and avoid blocking
+            headers = {
+                'user-agent': random.choice(USER_AGENT_LIST),
+                'accept-language': ACCEPT_LANGUAGE
+            }
+
+            # Make the HTTP GET request to fetch the webpage content
+            response = requests.get(self.url, headers=headers)
+
+            # Raise an exception if the request was unsuccessful
+            response.raise_for_status()
+
+            # Extract data from the webpage using the predefined YAML extractor
+            extractor = Extractor.from_yaml_file(YAML_FILE)
+            data = extractor.extract(response.text)
+           
+            # Check if the temperature element is present (walrus operator)
+            if temp := data.get('temperature') is None:
+                raise ElementException
+
+            # Extract the temperature value from the extracted data and convert it to float
+            temp = float(temp.split()[0])
+
+            return temp, None
+
+        except requests.RequestException as e:
+            # Handle exceptions related to the HTTP request
+            error_message = f"Error during HTTP request: {e}"
+
+        except ElementException:
+            # Handle exceptions when the temperature element is not found in the extracted data
+            error_message = "Temperature element not found."
+
+        except Exception as e:
+            # Handle any other exceptions that may occur
+            error_message = f"Error occurred: {e}"
+
+        return None, error_message
+
+
+if __name__ == "__main__":
+    
+    temp, error = Temperature("lebanon", "beirut").get()
+
+    if temp is None:
+        print(f"\n\n--- {error} ---\n\n")
+    else:
+        print(f"\n\n--- Temperature is: {temp} °C ---\n\n")
---
+++ constants.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+ASSETS = Path(__file__).parent / "assets"
+YAML_FILE = ASSETS / "resources" / "temp_selector.yml"
+
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+]
+
+ACCEPT_LANGUAGE = 'en-US,en;q=0.9'
+
+HEADERS = {
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+
+TEMPERATURE_XPATH = '//*[@id="qlook"]/div[3]'
+
+TEMPERATURE_SELECTOR = '#qlook > div.h2'
