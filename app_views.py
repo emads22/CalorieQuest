5c768d13601b55a408056cc6644b8ca1681e7050
Emads <emadsaab722@gmail.com>
2024-06-13 21:28:03 +0300

    complete implementing the Class Based Views

--- app_views.py
+++ app_views.py
@@ -1,6 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 from flask.views import MethodView
 from app_forms import CaloriesForm
+from classes import Temperature, Calorie
 
 
 class IndexView(MethodView):
@@ -14,4 +15,47 @@ class CaloriesFormView(MethodView):
         return render_template("calories_form.html", caloriesform=calories_form)
 
     def post(self):
-        pass
+        calories_form = CaloriesForm(request.form)
+        
+        if not calories_form.validate_on_submit():
+            for field, errors in calories_form.errors.items():
+                for error in errors:
+                    field_obj = getattr(calories_form, field, None)
+                    if field_obj is not None and hasattr(field_obj, 'label') and hasattr(field_obj.label, 'text'):
+                        field_label = field_obj.label.text
+                    else:
+                        field_label = field
+                    flash(f"Error in '{field_label}': {error}", 'danger')
+            # returns a 400 status code (Bad Request) to indicate that the client's request was incorrect and could not be processed.
+            return render_template('calories_form.html', caloriesform=calories_form), 400
+        
+
+        try:
+            # Extract data from the form
+            weight = float(calories_form.weight.data)
+            height = float(calories_form.height.data)
+            age = int(calories_form.age.data)
+
+            country = calories_form.country.data.strip().lower()
+            city = calories_form.city.data.strip().lower()
+            
+            
+            temperature, error = Temperature(country, city).get()
+            if temperature is None:
+                flash(error, 'danger')
+
+            calorie_intake = Calorie(weight=weight,
+                                     height=height,
+                                     age=age,
+                                     temperature=temperature).calculate()
+
+            # Render the result of the calculated calories intake of today in the same template, returns a 200 status code (OK) to indicate that the request was successful and the resource (in this case, the redirected page) is being returned.
+            return render_template(
+                'calories_form.html',
+                caloriesform=CaloriesForm(),
+                optimal_calorie_intake=calorie_intake), 200
+
+        except Exception as e:
+            flash(f"An error occurred while processing the form: {e}")
+            # Redirect to the form page if there's an error, return a 500 status code (Internal Server Error) to indicate that an unexpected error occurred on the server.
+            return redirect(url_for('calories_form_view')), 500
